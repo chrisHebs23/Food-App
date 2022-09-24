b124cd615d7eb268f6ff9d6c083200ff39fca1ac
chrisHebs23 <christopher.heberden@gmail.com>
2022-09-24 10:30:32 -0700

    Fisished Clasess on this app

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,12 +1,10 @@
-import { Text, View } from "react-native";
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
 import React, { useEffect, useState } from "react";
 import yelp from "../api/yelp";
 
 const ResultsShowScreen = ({ navigation }) => {
   const [result, setResult] = useState(null);
   const id = navigation.getParam("id");
-
-  console.log(result);
 
   const getResult = async (id) => {
     const response = await yelp.get(`/${id}`);
@@ -17,11 +15,29 @@ const ResultsShowScreen = ({ navigation }) => {
     getResult(id);
   }, []);
 
+  if (!result) {
+    return null;
+  }
+
   return (
     <View>
-      <Text>ResultsShowScreen</Text>
+      <Text>{result.name}</Text>
+      <FlatList
+        data={result.photos}
+        keyExtractor={(photo) => photo}
+        renderItem={({ item }) => {
+          return <Image style={styles.image} source={{ uri: item }} />;
+        }}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    width: 300,
+    height: 200,
+  },
+});
 
 export default ResultsShowScreen;
